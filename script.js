87dca7df4ed2d78a1f99461f6e952227d9546024
milesman34 <milesman034@gmail.com>
2024-11-10 15:12:11 -0500

    Finished tutorial

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
+// Adds a task
+const addTask = () => {
+    const taskInput = document.getElementById("taskInput");
 
+    if (taskInput.value) {
+        const taskList = document.getElementById("taskList");
+
+        // Create the new task
+        const newTask = document.createElement("li");
+
+        newTask.innerText = taskInput.value;
+
+        // Create the delete button
+        const deleteButton = document.createElement("button");
+
+        deleteButton.className = "button";
+        deleteButton.innerText = "x";
+
+        deleteButton.addEventListener("click", () => {
+            taskList.removeChild(newTask);
+        });
+
+        newTask.appendChild(deleteButton);
+
+        taskList.appendChild(newTask);
+
+        // Reset the input text
+        taskInput.value = "";
+    }
+};
+
+// Set up click listener for the button
+document.getElementById("submitButton").addEventListener("click", addTask);
+
+// Set up keyboard listener for the enter key
+document.getElementById("taskInput").addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+        addTask();
+    }
+});
